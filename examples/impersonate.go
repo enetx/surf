966336a01e7e25b6c3c5c470ae9846acd558b79e
x0xO <1815249-x0xO@users.noreply.gitlab.com>
2024-01-26 17:54:09 +0200

    move to go1.22, sync g

--- examples/impersonate.go
+++ examples/impersonate.go
@@ -12,7 +12,10 @@ func main() {
 	// url := "https://tls.peet.ws/api/all"
 	url := "https://chat.openai.com/"
 
-	opt := surf.NewOptions()
+	opt := surf.NewOptions().
+		// Proxy("http://127.0.0.1:18080")
+		// Proxy("http://127.0.0.1:2080")
+		Proxy("socks5://127.0.0.1:2080")
 
 	// opt.Impersonate().Chrome120()
 	opt.Impersonate().FireFox120()
