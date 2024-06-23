59c740421403fd946a8255f24160444ab1ac3b87
enetx <163449931+enetx@users.noreply.github.com>
2024-06-23 12:19:54 +0200

    sync, Chromium v126

--- impersonate.go
+++ impersonate.go
@@ -35,11 +35,11 @@ func (im *impersonate) Chrome() *builder {
 	//   ":authority: tls.peet.ws",
 	//   ":scheme: https",
 	//   ":path: /api/all",
-	//   "sec-ch-ua: \\\"Google Chrome\\\";v=\\\"125\\\", \\\"Chromium\\\";v=\\\"125\\\", \\\"Not.A/Brand\\\";v=\\\"24\\",
+	//   "sec-ch-ua: \\\"Not/A)Brand\\\";v=\\\"8\\\", \\\"Chromium\\\";v=\\\"126\\\", \\\"Google Chrome\\\";v=\\\"126\\",
 	//   "sec-ch-ua-mobile: ?0",
 	//   "sec-ch-ua-platform: \\\"Windows\\",
 	//   "upgrade-insecure-requests: 1",
-	//   "user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36",
+	//   "user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36",
 	//   "accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
 	//   "sec-fetch-site: none",
 	//   "sec-fetch-mode: navigate",
@@ -57,11 +57,11 @@ func (im *impersonate) Chrome() *builder {
 		Set(":scheme", "").
 		Set(":path", "").
 		Set(header.COOKIE, "").
-		Set(header.SEC_CH_UA, `"Google Chrome";v="125", "Chromium";v="125", "Not.A/Brand";v="24"`).
+		Set(header.SEC_CH_UA, `"Not/A)Brand";v="8", "Chromium";v="126", "Google Chrome";v="126"`).
 		Set(header.SEC_CH_UA_MOBILE, "?0").
 		Set(header.SEC_CH_UA_PLATFORM, `"Windows"`).
 		Set(header.UPGRADE_INSECURE_REQUESTS, "1").
-		Set(header.USER_AGENT, "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36").
+		Set(header.USER_AGENT, "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36").
 		Set(header.ACCEPT, "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7").
 		Set(header.SEC_FETCH_SITE, "none").
 		Set(header.SEC_FETCH_MODE, "navigate").
